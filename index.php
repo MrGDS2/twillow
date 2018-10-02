f780ec551ce6832a82fe541e46fbb6337e6fa58d
George Samuels II <mrsonicthunder@gmail.com>
2018-10-01 23:05:03 -0400

    new

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ require_once 'vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid    = "##";//Put your own credentials
-$token  = "##";
+$sid    = "AC35b0520a503e9ec60eb740564469dd60";//Put your own credentials
+$token  = "9f5062a71de0157a6cd3238a6825deb1";
 $twilio = new Client($sid, $token);
 
 $message = $twilio->messages
-    ->create("+11234567890", // to
+    ->create("+19175740612", // to
         array(
-            "from" => "+19876543210", //from
-            "body" => "Hello from Rohan Raygade CS 643 Fall 2018"
+            "from" => "+19085163288", //from
+            "body" => "Hello from George Samuels CS 643 Fall 2018"
         )
     );
 
