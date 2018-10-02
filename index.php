c16bebd8fb704edfd69b77a99da3d51cb8d959ce
George Samuels II <mrsonicthunder@gmail.com>
2018-10-01 23:28:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once 'vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid    = "AC35b0520a503e9ec60eb740564469dd60";//Put your own credentials
-$token  = "9f5062a71de0157a6cd3238a6825deb1";
+$sid    = "***";//Put your own credentials
+$token  = "***";
 $twilio = new Client($sid, $token);
 
 $message = $twilio->messages
