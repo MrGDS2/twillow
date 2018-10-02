659677abd7d4b49406866e7e1553ddd53fae50b7
George Samuels II <mrsonicthunder@gmail.com>
2018-10-01 23:19:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 echo ("getsms page ");
 // Update the path below to your autoload.php,
-// see https://getcomposer.org/doc/01-basic-usage.md
 require_once 'vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid    = "##";//Put your own credentials
-$token  = "##";
+$sid    = "AC35b0520a503e9ec60eb740564469dd60";//Put your own credentials
+$token  = "9f5062a71de0157a6cd3238a6825deb1";
 $twilio = new Client($sid, $token);
 
 $message = $twilio->messages
-    ->create("+19175740612", // to
+    ->create("+19173490168", // to
         array(
             "from" => "+19085163288", //from
             "body" => "Hello from George Samuels CS 643 Fall 2018"
@@ -19,3 +18,4 @@ $message = $twilio->messages
     );
 
 print($message->sid);
+?>
